e6897df0f3e6d4802397b319affeff081accf0ac
Deep Patel <deeppatel@live.com>
2020-10-06 02:03:53 -0400

    Functional Chart

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -23,3 +23,80 @@ var svg = d3
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+// Initial Params
+var chosenXAxis = "age";
+
+// function used for updating x-scale var upon click on axis label
+function xScale(Data, chosenXAxis) {
+  // create scales
+  var xLinearScale = d3.scaleLinear()
+    .domain([d3.min(Data, d => d[chosenXAxis]) *0.95,
+      d3.max(Data, d => d[chosenXAxis]) 
+    ])
+    .range([0, width]);
+
+  return xLinearScale;
+}
+
+// Retrieve data from the CSV file and execute everything below
+d3.csv("assets/data/data.csv").then(function(Data, err) {
+  if (err) throw err;
+  console.log(Data);
+
+  // parse data
+  Data.forEach(function(data) {
+    data.age = +data.age;
+    data.smokes = +data.smokes;
+  });
+
+  // xLinearScale function above csv import
+  var xLinearScale = xScale(Data, chosenXAxis);
+
+  // Create y scale function
+  var yLinearScale = d3.scaleLinear()
+    .domain([8, d3.max(Data, d => d.smokes)])
+    .range([height, 0]);
+
+  // Create initial axis functions
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
+
+  // append x axis
+  chartGroup.append("g")
+    .classed("x-axis", true)
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+  chartGroup.append("text")
+    .attr("y", 435)
+    .attr("x", 450)
+    .attr("dy", "1em")
+    .classed("active", true)
+    .text("Age");
+
+    // append y axis
+  chartGroup.append("g")
+    .call(leftAxis);
+
+  chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .classed("active", true)
+    .text("Smokers");
+
+  // append initial circles
+  chartGroup.selectAll("circle")
+    .data(Data)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d[chosenXAxis]))
+    .attr("cy", d => yLinearScale(d.smokes))
+    .attr("r", 15)
+    .attr("fill", "skyblue")
+    .attr("opacity", ".5");
+
+}).catch(function(error) {
+  console.log(error);
+});
